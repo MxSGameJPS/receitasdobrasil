49cacd9ffe0153639f5104a1766f100f76825903
MxSGameJPS <mxsgamejps@gmail.com>
2025-03-23 06:40:07 -0300

    Remove comentário desnecessário sobre redirecionamento no arquivo home.jsx

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom"; // Adicionado pra redirecionar
+import { useNavigate } from "react-router-dom"; 
 import styled from "styled-components";
 
 export default function Home() {
